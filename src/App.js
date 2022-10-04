30d660832bc81f3af13aa7596bf3db0ff2012a49
John Oduor Otieno <johnoduor222@gmail.com>
2022-10-04 05:28:13 -0700

    Updated

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {Routes,Route,Navigate} from "react-router-dom"
 
 
 
+
 function App() {
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -13,8 +15,4 @@ root.render(
     </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
