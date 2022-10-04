3d0a83bcc421cda3e7aeb7cd7b2d2db156d18bd8
John Oduor Otieno <johnoduor222@gmail.com>
2022-10-04 03:11:37 -0700

    Added the header component to be returned

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './index.css';
+import Header from './components/Header';
 
 
 function App() {
   return (
     <div>
+    <Header/>
       <h1>hello world</h1>
     </div>
   )
