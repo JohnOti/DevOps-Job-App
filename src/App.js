b1d7e94e8eecca168f46414a8883ad6c4093346a
John Oduor Otieno <johnoduor222@gmail.com>
2022-10-06 13:28:29 -0700

    Added AddJobForm component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 
 import './index.css';
 import React, { useState,useEffect} from 'react'
-import Form from './components/Form';
+import AddJobForm from './components/AddJobForm';
 import Header from './components/Header';
 import JobLists from './components/JobLists';
 import JobDetails from './components/JobDetails';
 import {Routes,Route,Navigate,} from "react-router-dom"
 function App() {
+  const url="https://devops-job-app.herokuapp.com/jobs"
   const[jobs,setJobs]=useState([])
   useEffect(()=>{
-    fetch("https://devops-job-app.herokuapp.com/jobs")
+    fetch(url)
     .then(res=>res.json())
     .then(data=>{
     setJobs(data)
@@ -24,7 +25,7 @@ function App() {
       <Route path="/" element={<Navigate to="/jobs"/>}/>
       <Route path="/jobs" element={<JobLists jobs={jobs}/>}/>
       <Route path="/jobs/:position" element={<JobDetails jobs={jobs}/>}/>
-      <Route path="/jobs/add" element={<Form/>}/>
+      <Route path="/jobs/add" element={<AddJobForm job={jobs} url={url} setJobs={setJobs} />}/>
     </Routes>
     </div>
   )
