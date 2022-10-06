2c7fb9c3b47cfd25e9474e9809e543467d787fd7
John Oduor Otieno <johnoduor222@gmail.com>
2022-10-06 10:12:22 -0700

    Added routes and added fetch method

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-// import logo from './logo.svg';
+
 import './index.css';
+import React, { useState,useEffect} from 'react'
+import Form from './components/Form';
 import Header from './components/Header';
 import JobLists from './components/JobLists';
 import JobDetails from './components/JobDetails';
-import {Routes,Route,Navigate} from "react-router-dom"
+import {Routes,Route,Navigate,} from "react-router-dom"
 function App() {
+  const[jobs,setJobs]=useState([])
+  useEffect(()=>{
+    fetch("https://devops-job-app.herokuapp.com/jobs")
+    .then(res=>res.json())
+    .then(data=>{
+    setJobs(data)
+    console.log(data)
+    })
+  },[])
   return (
     <div>
+      
     <Header/>
     <Routes>
       <Route path="/" element={<Navigate to="/jobs"/>}/>
-      <Route path="/jobs" element={<JobLists />}/>
-      <Route path="/jobs/:position" element={<JobDetails/>}/>
+      <Route path="/jobs" element={<JobLists jobs={jobs}/>}/>
+      <Route path="/jobs/:position" element={<JobDetails jobs={jobs}/>}/>
+      <Route path="/jobs/add" element={<Form/>}/>
     </Routes>
     </div>
   )
